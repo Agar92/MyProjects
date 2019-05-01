f8b2b46a7764ea135210bc267263b5863a4e7c77
Andrey <agar10@yandex.ru>
2019-05-01 16:03:01 +0400

    Added primitive console snake game.

---
+++ PrimitiveConsoleSnakeGame/main.cpp
@@ -0,0 +1,17 @@
+#include "Snake.h"
+#include <QApplication>
+
+#include <QApplication>
+#include "snake.h"
+
+int main(int argc, char *argv[]) {
+
+  QApplication app(argc, argv);
+
+  Snake window;
+
+  window.setWindowTitle("Snake");
+  window.show();
+
+  return app.exec();
+}
